d3bc4e38c9c10363c783e8076a72049efa869c30
pebridayanti <pebridayantihasibuan@gmail.com>
2018-08-21 14:30:33 +0700

    Update segitiga.py

--- segitiga.py
+++ segitiga.py
@@ -3,16 +3,15 @@ bar = 1
 
 x = int(input("Masukkan angka :"))
 
-#looping Baris
+# Looping Baris
 while bar <= x:
 	kol = bar
 
 	# Looping Kolom
 	while kol > 0:
 		string = string + " * "
-		kol = kol - 1 
-
-		string = string + "\n"
-		bar =  bar + 1
-
+		kol = kol - 1
+		
+	string = string + "\n"
+	bar = bar + 1
 print (string)
